a1e8e24c27e6eeaa230d0df788d0f96d81a0ae29
iceice666 <iceice666@outlook.com>
2024-10-31 15:44:14 +0800

    refactor player implementation
    - use channel to send/recv request and response
    - handle received events
    - streamline `update_current_track`'s logic and support `prev track`
    operation
    - extract source appending logic to a function

--- player/src/_impl.rs
+++ player/src/_impl.rs
@@ -1,15 +1,12 @@
 use rand::prelude::*;
 use rodio::{OutputStream, Sink};
 use std::{
-    collections::{HashMap, VecDeque},
     fmt::Debug,
+    sync::mpsc::{channel, Receiver, Sender},
 };
-use tracing::{debug, warn};
+use tracing::{debug, info, trace, warn};
 
-use crate::{
-    error::{PlayerError, PlayerResult},
-    TrackObject, TrackSource,
-};
+use crate::{error::PlayerResult, TrackObject, TrackSource};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum RepeatState {
@@ -18,22 +15,52 @@ pub enum RepeatState {
     None,
 }
 
+#[derive(Debug)]
+pub enum EventRequest {
+    Play,
+    Pause,
+    Stop,
+    Next,
+    Prev,
+    // Seek(u64),
+    GetVolume,
+    SetVolume(f32),
+    GetRepeat,
+    SetRepeat(RepeatState),
+    ToggleShuffle,
+
+    NewTrack(TrackObject),
+}
+
+#[derive(Debug)]
+pub enum EventResponse {
+    Volume(f32),
+    Repeat(RepeatState),
+    Ok,
+    Error(String),
+}
+
 pub struct Player {
-    playlist: VecDeque<TrackObject>,
-    current_track: Option<TrackObject>,
+    playlist: Vec<TrackObject>,
+    current_track_index: usize,
+    is_playing: bool,
     sink: Sink,
     __stream: OutputStream,
+
+    __event_queue_receiver: Receiver<EventRequest>,
+    __event_response_sender: Sender<EventResponse>,
 
     // Flags
     repeat: RepeatState,
     is_shuffle: bool,
+    is_playlist_going_backwards: bool,
 }
 
 impl Debug for Player {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Player")
             .field("playlist", &self.playlist)
-            .field("current_track", &self.current_track)
+            .field("current_track", &self.current_track_index)
             .field("repeat", &self.repeat)
             .field("is_shuffle", &self.is_shuffle)
             .finish()
@@ -41,78 +68,127 @@ impl Debug for Player {
 }
 
 impl Player {
-    pub fn try_new() -> PlayerResult<Self> {
+    pub fn try_new() -> PlayerResult<(Self, Sender<EventRequest>, Receiver<EventResponse>)> {
         let (__stream, stream_handle) = OutputStream::try_default()?;
         let sink = Sink::try_new(&stream_handle)?;
+        let (event_queue_tx, event_queue_rx) = channel();
+        let (event_response_tx, event_response_rx) = channel();
 
-        Ok(Self {
-            playlist: VecDeque::new(),
-            current_track: None,
+        let this = Self {
+            playlist: Vec::new(),
+            current_track_index: 0,
+            is_playing: false,
             sink,
             __stream,
 
+            __event_queue_receiver: event_queue_rx,
+            __event_response_sender: event_response_tx,
+
             repeat: RepeatState::None,
             is_shuffle: false,
-        })
+            is_playlist_going_backwards: false,
+        };
+
+        Ok((this, event_queue_tx, event_response_rx))
     }
 
-    #[inline]
-    pub fn add_track(&mut self, track: TrackObject) {
-        self.playlist.push_back(track);
-    }
+    pub fn mainloop(&mut self) {
+        info!("Starting mainloop");
+        loop {
+            self.dispatch_request();
 
-    #[tracing::instrument]
-    pub fn mainloop(mut self) -> PlayerResult {
-        debug!("Starting mainloop");
-        while !self.playlist.is_empty() {
-            self.update_current_track();
-
-            let Some(ref mut track) = self.current_track else {
+            if !self.is_playing || !self.sink.empty() {
                 continue;
-            };
-            debug!("Next track: {}", track.get_unique_id());
-
-            debug!("Building source");
-            let source = match track.build_source() {
-                Ok(source) => source,
-                Err(e) => {
-                    warn!(
-                        "Failed to build source for track {}: {}",
-                        track.get_unique_id(),
-                        e
-                    );
-                    continue;
-                }
-            };
-
-            debug!("Appending source to sink");
-            match source {
-                TrackSource::F32(source) => self.sink.append(source),
-                TrackSource::I16(source) => self.sink.append(source),
-                TrackSource::U16(source) => self.sink.append(source),
             }
 
-            debug!("Waiting for track to end");
-            self.sink.sleep_until_end();
-        }
+            self.update_current_track();
 
-        Ok(())
+            if !self.is_playing {
+                continue;
+            }
+
+            self.append_source();
+        }
     }
 
     #[inline]
     fn update_current_track(&mut self) {
-        if self.repeat == RepeatState::Track && self.current_track.is_some() {
-            // nop
-        } else if self.repeat == RepeatState::Queue && self.current_track.is_some() {
-            self.playlist.push_back(self.current_track.take().unwrap());
-            self.current_track = self.playlist.pop_front();
-        } else if self.is_shuffle {
-            let mut rng = rand::thread_rng();
-            let index = rng.gen_range(0..self.playlist.len());
-            self.playlist.swap(0, index);
-            self.current_track = self.playlist.pop_front();
-        } else {
-            self.current_track = self.playlist.pop_front();
+        let reverse = self.is_playlist_going_backwards;
+        let playlist_len = self.playlist.len();
+
+        let idx = self.current_track_index;
+        self.current_track_index = match (self.is_shuffle, self.repeat) {
+            (true, _) => thread_rng().gen_range(0..playlist_len),
+            (_, RepeatState::Track) => self.current_track_index,
+            (_, RepeatState::Queue) => {
+                let next_idx = idx + if reverse { playlist_len - 1 } else { 1 };
+                next_idx % playlist_len
+            }
+            (_, RepeatState::None) => {
+                let next_idx = idx + if reverse { playlist_len - 1 } else { 1 };
+                if next_idx >= playlist_len {
+                    self.is_playing = false;
+                    next_idx % (playlist_len + 1) // remain a new space for the new track
+                } else {
+                    next_idx
+                }
+            }
+        };
+
+        self.is_playlist_going_backwards = false;
+        trace!(
+            "Player state: index:{}, is_playing:{}, is_shuffle:{}, repeat:{:?}",
+            self.current_track_index,
+            self.is_playing,
+            self.is_shuffle,
+            self.repeat
+        )
+    }
+
+    fn dispatch_request(&mut self) {
+        if let Ok(request) = self.__event_queue_receiver.try_recv() {
+            debug!("Received request: {:?}", request);
+
+            match request {
+                EventRequest::Play => {
+                    self.sink.play();
+                    self.is_playing = true;
+                }
+                EventRequest::Pause => self.sink.pause(),
+                EventRequest::Stop => {
+                    self.sink.stop();
+                    self.is_playing = false;
+                }
+                EventRequest::Next => self.sink.stop(),
+                EventRequest::Prev => {
+                    self.is_playlist_going_backwards = true;
+                    self.sink.stop();
+                }
+                EventRequest::GetVolume => {
+                    let volume = self.sink.volume();
+                    self.__event_response_sender
+                        .send(EventResponse::Volume(volume))
+                        .unwrap()
+                }
+                EventRequest::SetVolume(volume) => self.sink.set_volume(volume),
+                EventRequest::GetRepeat => self
+                    .__event_response_sender
+                    .send(EventResponse::Repeat(self.repeat))
+                    .unwrap(),
+                EventRequest::SetRepeat(repeat) => self.repeat = repeat,
+                EventRequest::ToggleShuffle => self.is_shuffle = !self.is_shuffle,
+                EventRequest::NewTrack(track) => {
+                    self.playlist.push(track);
+
+                    if !self.is_playing {
+                        self.is_playing = true;
+
+                        if self.current_track_index == self.playlist.len() - 1 {
+                            self.append_source();
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -128,35 +204,37 @@ impl Player {
         Ok(())
     }
 
-    // Interface methods
-
     #[inline]
-    pub fn set_volume(&mut self, volume: f32) {
-        self.sink.set_volume(volume);
-    }
+    fn append_source(&mut self) {
+        let track = self.playlist.get(self.current_track_index).unwrap();
+        info!(
+            "Next track:[{}] {}",
+            self.current_track_index,
+            track.get_unique_id()
+        );
 
-    #[inline]
-    pub fn get_volume(&self) -> f32 {
-        self.sink.volume()
-    }
+        trace!("Building source");
+        let source = match track.build_source() {
+            Ok(source) => source,
+            Err(e) => {
+                let err_msg = format!(
+                    "Failed to build source for track {}: {}",
+                    track.get_unique_id(),
+                    e
+                );
+                warn!("{}", err_msg);
+                self.__event_response_sender
+                    .send(EventResponse::Error(err_msg))
+                    .unwrap();
+                return;
+            }
+        };
 
-    #[inline]
-    pub fn toggle_shuffle(&mut self) {
-        self.is_shuffle = !self.is_shuffle;
-    }
-
-    #[inline]
-    pub fn toggle_repeat(&mut self) {
-        match self.repeat {
-            RepeatState::None => self.repeat = RepeatState::Queue,
-            RepeatState::Queue => self.repeat = RepeatState::Track,
-            RepeatState::Track => self.repeat = RepeatState::None,
+        trace!("Appending source to sink");
+        match source {
+            TrackSource::F32(source) => self.sink.append(source),
+            TrackSource::I16(source) => self.sink.append(source),
+            TrackSource::U16(source) => self.sink.append(source),
         }
-    }
-
-    #[inline]
-    pub fn get_info(&self) -> PlayerResult<HashMap<String, String>> {
-        let track = self.current_track.as_ref().ok_or(PlayerError::EmptyTrack)?;
-        track.info()
     }
 }
--- player/src/lib.rs
+++ player/src/lib.rs
@@ -2,7 +2,7 @@ mod _impl;
 pub mod error;
 use std::{collections::HashMap, fmt::Debug};
 
-pub use _impl::Player;
+pub use _impl::*;
 use error::PlayerResult;
 
 pub type TrackInfo = HashMap<String, String>;
@@ -26,9 +26,6 @@ pub type TrackObject = Box<dyn Track>;
 
 impl Debug for TrackObject {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("TrackObject")
-            .field("info", &self.info())
-            .field("unique_id", &self.get_unique_id())
-            .finish()
+        write!(f, "TrackObject({})", self.get_unique_id())
     }
 }
