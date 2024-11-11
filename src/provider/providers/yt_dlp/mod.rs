b1c5d1a0684e28ba30c0f5561fa14e89bd1d066d
iceice666 <iceice666@outlook.com>
2024-11-11 16:55:01 +0800

    feat(yt-dlp): support URL specified downloads

--- src/provider/providers/yt_dlp/mod.rs
+++ src/provider/providers/yt_dlp/mod.rs
@@ -1,6 +1,8 @@
 pub mod utils;
 
-use crate::provider::error::ProviderResult;
+use regex::Regex;
+
+use crate::provider::error::{ProviderError, ProviderResult};
 use crate::provider::providers::yt_dlp::utils::run_cmd;
 use crate::provider::sources::local_file::LocalFileTrack;
 use crate::provider::sources::TrackObject;
@@ -8,12 +10,15 @@ use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 use std::fs::{self, OpenOptions};
 use std::io::Write;
+use std::sync::LazyLock;
 
 #[derive(Debug)]
 pub enum SearchPlatform {
     Youtube,
     SoundCloud,
     BiliBili,
+
+    UrlSpecified(String),
 }
 
 impl Display for SearchPlatform {
@@ -22,6 +27,7 @@ impl Display for SearchPlatform {
             Self::Youtube => "ytsearch",
             Self::SoundCloud => "scsearch",
             Self::BiliBili => "bilisearch",
+            Self::UrlSpecified(_) => "url",
         };
 
         write!(f, "{}", str)
@@ -49,7 +55,7 @@ pub struct DownloadOption {
 
 impl Display for DownloadOption {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self.platform {
+        match &self.platform {
             SearchPlatform::Youtube => {
                 write!(f, "https://www.youtube.com/watch?v={}", self.video_id)
             }
@@ -59,12 +65,27 @@ impl Display for DownloadOption {
             SearchPlatform::BiliBili => {
                 write!(f, "https://www.bilibili.com/video/{}", self.video_id)
             }
+            SearchPlatform::UrlSpecified(url) => {
+                write!(f, "{}", url)
+            }
         }
     }
 }
 
 #[derive(Debug)]
 pub struct YtDlp;
+
+const INDEX_CSV: &str = "audio/yt-dlp/index.csv";
+const OUTPUT_TEMPLATE: &str = "audio/yt-dlp/%(extractor_key)s/%(fulltitle)s.%(ext)s";
+
+static YOUTUBE_REGEX: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^https://www.youtube.com/watch\?.*(?:v=([^&?]*)).*$").unwrap());
+
+static YOUTU_BE_REGEX: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^https://youtu.be/([^?]*).*$").unwrap());
+
+static BILIBILI_REGEX: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^https://www.bilibili.com/video/([^/?]*).*$").unwrap());
 
 impl YtDlp {
     pub fn try_new() -> ProviderResult<Self> {
@@ -102,25 +123,59 @@ impl YtDlp {
     }
 
     pub async fn download(&self, query: DownloadOption) -> ProviderResult<TrackObject> {
+        // Try parse to platform specific source
+        let query = self.parse_download_query(query);
+
+        let need_update_index = !matches!(query.platform, SearchPlatform::UrlSpecified(_));
+
         // Try to find existing download first
-        if let Some(track) = self.find_existing_track(&query)? {
-            return Ok(track);
-        }
+        match self.find_existing_track(&query) {
+            Ok(Some(track)) => return Ok(track),
+            Ok(None) | Err(ProviderError::Io(_)) => {}
+            Err(e) => return Err(e),
+        };
 
         // Download new track
         let output = self.download_track(&query.to_string())?;
 
-        // Update index
-        self.update_index(&query, &output)?;
+        // Update index if needed
+        if need_update_index {
+            self.update_index(&query, &output)?;
+        }
 
         Ok(Box::new(LocalFileTrack::new(output)))
     }
 
-    const INDEX_CSV: &str = "music/yt-dlp/index.csv";
-    const OUTPUT_TEMPLATE: &str = "music/yt-dlp/%(extractor_key)s/%(fulltitle)s.%(ext)s";
+    fn parse_download_query(&self, query: DownloadOption) -> DownloadOption {
+        let SearchPlatform::UrlSpecified(ref url) = query.platform else {
+            return query;
+        };
+
+        if let Some(cap) = YOUTUBE_REGEX.captures(url) {
+            let video_id = cap.get(1).unwrap().as_str().to_string();
+            DownloadOption {
+                platform: SearchPlatform::Youtube,
+                video_id,
+            }
+        } else if let Some(cap) = YOUTU_BE_REGEX.captures(url) {
+            let video_id = cap.get(1).unwrap().as_str().to_string();
+            DownloadOption {
+                platform: SearchPlatform::Youtube,
+                video_id,
+            }
+        } else if let Some(cap) = BILIBILI_REGEX.captures(url) {
+            let video_id = cap.get(1).unwrap().as_str().to_string();
+            DownloadOption {
+                platform: SearchPlatform::BiliBili,
+                video_id,
+            }
+        } else {
+            query
+        }
+    }
 
     fn find_existing_track(&self, query: &DownloadOption) -> ProviderResult<Option<TrackObject>> {
-        let csv_content = fs::read_to_string(Self::INDEX_CSV)?;
+        let csv_content = fs::read_to_string(INDEX_CSV)?;
         let csv_query = format!("{},{}", query.video_id, query.platform);
 
         csv_content
@@ -146,13 +201,16 @@ impl YtDlp {
             "--print",
             "after_move:filepath",
             "--output",
-            Self::OUTPUT_TEMPLATE,
+            OUTPUT_TEMPLATE,
             url,
         ])
     }
 
     fn update_index(&self, query: &DownloadOption, output: &str) -> ProviderResult<()> {
-        let mut file = OpenOptions::new().append(true).open(Self::INDEX_CSV)?;
+        let mut file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(INDEX_CSV)?;
         writeln!(file, "{},{},{}\n", query.video_id, query.platform, output)?;
         Ok(())
     }
@@ -169,7 +227,7 @@ mod tests {
         let query = SearchOption {
             platform: SearchPlatform::Youtube,
             len: 5,
-            keyword: "maimai world's end loneliness".to_string(),
+            keyword: "ongeki super ambulance".to_string(),
         };
 
         let result = dlp.search(query).await?;
@@ -212,7 +270,7 @@ mod tests {
         let query = SearchOption {
             platform: SearchPlatform::BiliBili,
             len: 1,
-            keyword: "ak+q".to_string(),
+            keyword: "まふまふ".to_string(),
         };
 
         let result = dlp.search(query).await?;
@@ -221,6 +279,20 @@ mod tests {
         let query = DownloadOption {
             platform: SearchPlatform::BiliBili,
             video_id: vid.to_string(),
+        };
+        dlp.download(query).await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_url() -> anyhow::Result<()> {
+        let dlp = YtDlp::try_new()?;
+
+        let query = DownloadOption {
+            platform: SearchPlatform::UrlSpecified(String::from(
+                "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            )),
+            video_id: String::new(),
         };
         dlp.download(query).await?;
         Ok(())
