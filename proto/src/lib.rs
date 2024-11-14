f8571e8d7601b39d4904124b8c9c6ada8b86978a
iceice666 <iceice666@outlook.com>
2024-11-14 21:15:38 +0800

    feat(proto): add `PlayerState` field

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -4,9 +4,9 @@ pub use prost::DecodeError;
 use prost::Message;
 
 pub use proto::{
-    request::Payload as RequestPayload, response::Payload as ResponsePayload, ProviderConfig,
-    ProviderList, RepeatState, Request as PlayerRequest, RequestType, Response as PlayerResponse,
-    ResponseType, SearchResults, TrackConfig, TrackData, TrackSearch,
+    request::Payload as RequestPayload, response::Payload as ResponsePayload, PlayerState,
+    ProviderConfig, ProviderList, RepeatState, Request as PlayerRequest, RequestType,
+    Response as PlayerResponse, ResponseType, SearchResults, TrackConfig, TrackData, TrackSearch,
 };
 
 pub fn serialize_response(response: PlayerResponse) -> Vec<u8> {
