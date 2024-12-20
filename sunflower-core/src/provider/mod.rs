9150ad859fe953a9cbd68fbbb2a174004cb59463
iceice666 <iceice666@outlook.com>
2024-12-20 22:01:53 +0800

    refactor: remove feature flags

--- sunflower-core/src/provider/mod.rs
+++ sunflower-core/src/provider/mod.rs
@@ -1,15 +1,13 @@
-pub mod sinewave;
-
 mod error;
-mod local_file;
-pub mod ytdl;
+pub(crate) mod local_file;
+pub(crate) mod sinewave;
+pub(crate) mod ytdl;
 
 use crate::provider::error::{ProviderError, ProviderResult};
 use crate::provider::local_file::LocalFileProvider;
 use crate::provider::sinewave::SineWaveProvider;
 use crate::provider::ytdl::YtdlProvider;
 use crate::source::SourceKinds;
-use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use tracing::error;
 
@@ -37,34 +35,34 @@ pub trait ProviderTrait {
 macro_rules! define_provider_kinds {
     (
         $f_name:ident=>$f_clz:ident
-        $(,#[$feature:literal] $name:ident=>$clz:ident)*
+        $(, $name:ident=>$clz:ident)*
 
     ) => {
         #[derive(Debug, Eq, PartialEq)]
         pub enum ProviderKinds{
             $f_name($f_clz)
-            $(,#[cfg(feature=$feature)] $name ($clz))*
+            $(, $name ($clz))*
         }
 
         impl ProviderTrait for ProviderKinds {
             fn get_name(&self) -> String {
                 match self {
                     Self::$f_name(kind) => kind.get_name()
-                    $(,#[cfg(feature=$feature)] Self::$name(kind) => kind.get_name())*
+                    $(, Self::$name(kind) => kind.get_name())*
                 }
             }
 
             fn search(&mut self, term:&str, max_results: Option<usize>) -> SearchResult {
                 match self {
                     Self::$f_name(kind) => kind.search(term, max_results)
-                    $(,#[cfg(feature=$feature)] Self::$name(kind) => kind.search(term, max_results))*
+                    $(, Self::$name(kind) => kind.search(term, max_results))*
                 }
             }
 
             fn get_track(&self,input: &str) -> ProviderResult<SourceKinds> {
                 match self {
                     Self::$f_name(kind) => kind.get_track(input)
-                    $(,#[cfg(feature=$feature)] Self::$name(kind) => kind.get_track(input))*
+                    $(, Self::$name(kind) => kind.get_track(input))*
                 }
             }
         }
@@ -74,10 +72,8 @@ macro_rules! define_provider_kinds {
 define_provider_kinds! {
     Sinewave => SineWaveProvider,
 
-    #["provider-local_file"]
     LocalFile => LocalFileProvider,
 
-    #["provider-yt-dlp"]
     Ytdl => YtdlProvider
 }
 
@@ -109,11 +105,6 @@ impl ProviderRegistry {
 
     pub fn unregister(&mut self, name: &str) {
         self.providers.remove(name);
-    }
-
-    pub fn create(&mut self, fields: ProviderFields) -> ProviderResult<()> {
-        self.register(fields.try_into()?);
-        Ok(())
     }
 
     pub fn search(
@@ -149,37 +140,5 @@ impl ProviderRegistry {
             Some(provider) => provider.get_track(id),
             None => Err(ProviderError::ProviderNotFound(provider.to_string())),
         }
-    }
-}
-
-/// Used for create new provider
-#[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
-pub enum ProviderFields {
-    Sinewave,
-
-    #[cfg(feature = "provider-local_file")]
-    LocalFile {
-        music_folder: String,
-    },
-
-    #[cfg(feature = "provider-yt-dlp")]
-    Ytdl,
-}
-
-impl TryFrom<ProviderFields> for ProviderKinds {
-    type Error = ProviderError;
-
-    fn try_from(fields: ProviderFields) -> Result<Self, Self::Error> {
-        Ok(match fields {
-            ProviderFields::Sinewave => ProviderKinds::Sinewave(SineWaveProvider),
-
-            #[cfg(feature = "provider-local_file")]
-            ProviderFields::LocalFile { music_folder } => {
-                ProviderKinds::LocalFile(LocalFileProvider::new(music_folder))
-            }
-
-            #[cfg(feature = "provider-yt-dlp")]
-            ProviderFields::Ytdl => ProviderKinds::Ytdl(YtdlProvider::try_new()?),
-        })
     }
 }
