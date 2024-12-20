7b9f74a5b9f580189add5b019697ce09bb644d80
iceice666 <iceice666@outlook.com>
2024-12-20 21:56:34 +0800

    refactor(protocol): use protobuf and remove old handler

--- sunflower-core/src/protocol/mod.rs
+++ sunflower-core/src/protocol/mod.rs
@@ -1,5 +1,57 @@
-mod request;
-mod response;
+use crate::player::Repeat;
+use crate::provider::local_file::LocalFileProvider;
+use crate::provider::ytdl::YtdlProvider;
+use crate::provider::*;
 
-pub use request::*;
-pub use response::*;
+pub(crate) mod proto {
+    tonic::include_proto!("player.v1");
+}
+
+impl From<Repeat> for proto::player_state::RepeatMode {
+    fn from(value: Repeat) -> Self {
+        match value {
+            Repeat::None => Self::RepeatOff,
+            Repeat::Track => Self::RepeatOne,
+            Repeat::Queue => Self::RepeatAll,
+        }
+    }
+}
+
+impl From<proto::player_state::RepeatMode> for Repeat {
+    fn from(value: proto::player_state::RepeatMode) -> Self {
+        match value {
+            proto::player_state::RepeatMode::RepeatOff => Repeat::None,
+            proto::player_state::RepeatMode::RepeatOne => Repeat::Track,
+            proto::player_state::RepeatMode::RepeatAll => Repeat::Queue,
+        }
+    }
+}
+
+impl TryFrom<proto::register_provider_request::Provider> for ProviderKinds {
+    type Error = String;
+
+    fn try_from(value: proto::register_provider_request::Provider) -> Result<Self, Self::Error> {
+        match value {
+            proto::register_provider_request::Provider::Sinewave(proto::SineWaveProvider {}) => {
+                Ok(ProviderKinds::Sinewave(Default::default()))
+            }
+
+            proto::register_provider_request::Provider::LocalFile(proto::LocalFileProvider {
+                music_folder,
+                recursive_scan,
+            }) => Ok(ProviderKinds::LocalFile(LocalFileProvider::new(
+                music_folder,
+                recursive_scan,
+            ))),
+
+            proto::register_provider_request::Provider::Ytdl(proto::YtdlProvider {
+                binary_path: Some(binary_path),
+                extra_args,
+            }) => Ok(ProviderKinds::Ytdl(
+                YtdlProvider::try_new(binary_path, extra_args).map_err(|e| e.to_string())?,
+            )),
+
+            _ => Err(String::from("Unsupported provider type")),
+        }
+    }
+}
