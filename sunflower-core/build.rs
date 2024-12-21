e45b918239d1db26fd296378bc23ecd1206e8259
iceice666 <iceice666@outlook.com>
2024-12-21 15:10:46 +0800

    feat(build): add git hash retrieval and include in build output

--- sunflower-core/build.rs
+++ sunflower-core/build.rs
@@ -1,4 +1,32 @@
+use std::{env, fs, path::Path, process::Command};
+
 fn main() {
     tonic_build::compile_protos("protocol.proto")
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+
+    get_commit_hash();
+}
+
+fn get_commit_hash() {
+    // Get the git hash
+    let output = Command::new("git")
+        .args(["rev-parse", "HEAD"])
+        .output()
+        .unwrap();
+    let git_hash = String::from_utf8(output.stdout).unwrap();
+
+    // Create the output directory if it doesn't exist
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("git_hash.rs");
+
+    // Write the git hash to a file that will be included in the build
+    fs::write(
+        dest_path,
+        format!("pub const GIT_HASH: &str = \"{}\";", git_hash.trim()),
+    )
+    .unwrap();
+
+    // Tell cargo to rerun this script only if we have new commits
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-changed=.git/refs");
 }
--- sunflower-core/src/lib.rs
+++ sunflower-core/src/lib.rs
@@ -7,16 +7,24 @@ pub(crate) mod source;
 pub(crate) mod utils;
 
 pub use crate::daemon::Daemon;
-pub use crate::protocol::{
-    PlayerRequest, PlayerStateRequest, ProviderRequest, Request, RequestKind, Response,
-    ResponseKind, TrackRequest,
-};
-pub use crate::provider::ProviderFields;
 
 pub fn init_logger() {
+    #[cfg(debug_assertions)]
     let _ = tracing_subscriber::fmt()
         .with_max_level(tracing::Level::TRACE)
         .with_file(true)
         .with_line_number(true)
         .try_init();
+
+    #[cfg(not(debug_assertions))]
+    let _ = tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_file(false)
+        .with_line_number(false)
+        .try_init();
+}
+
+
+pub mod version {
+    include!(concat!(env!("OUT_DIR"), "/git_hash.rs"));
 }
